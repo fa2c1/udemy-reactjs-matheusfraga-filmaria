c175485d81f7eaf81c6e1f35c441e6cb492472da
Fernando Coutinho <fa2c1@hotmail.com>
2022-02-25 21:57:36 -0300

    Favoritos Page

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,44 @@
 import './styles.css';
+import { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom';
 
 function Favoritos(){
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=> {
+
+        const minhaLista = localStorage.getItem('filmes');
+        setFilmes(JSON.parse(minhaLista) || []);
+
+    }, []);
+
+    function handleDelete(id){
+        let filtroFilmes = filmes.filter((item)=>{
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem('filmes', JSON.stringify(filtroFilmes))
+    }
+
     return(
-        <div>Pagina Filmes Favoritos</div>
+        <div id="meus-filmes">
+            <h1>Meus Filmes</h1>
+            <ul>
+                {filmes.map((item)=>{
+                    return(
+                        <li key={item.is}>
+                            <span>{item.nome}</span>
+                            <div>
+                                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                                <button onClick={ () => handleDelete(item.id) }>Excluir</button>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
     );
 }
 
